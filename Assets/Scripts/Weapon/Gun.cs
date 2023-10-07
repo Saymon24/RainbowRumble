348ed07cdeb11437dd01c741980563c6dfaefec3
Simon A <simon.auduberteau@epitech.eu>
2023-10-07 14:56:30 +0900

    [+] New partciles effect on NARF's muzzleFlash + add impact force on boxes for testing purposes

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -8,25 +8,43 @@ public class Gun : MonoBehaviour
     [Header("Gun Settings")]
     [SerializeField] float damage = 10f;
     [SerializeField] float range = 100f;
+    [SerializeField] float firerate = 200f;
+    [SerializeField] float impactForce = 10f;
+    [SerializeField] Transform gunEnd;
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] GameObject impactEffect;
 
     [SerializeField] Camera fpsCam;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / firerate;
             Shoot();
         }
     }
 
     private void Shoot()
     {
+
+        muzzleFlash.GetComponent<ParticleSystem>().Play();
+
         RaycastHit hit;
 
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            print(hit.transform.name);
+
+
+            if (hit.transform.gameObject.CompareTag("Box"))
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+
         }
     }
 }

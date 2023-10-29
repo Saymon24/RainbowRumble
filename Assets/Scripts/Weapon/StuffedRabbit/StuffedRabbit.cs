108357c1d312d4bedb226cebb8efee9418ec0a20
Alexis Devloo <72014883+AlexisDevloo@users.noreply.github.com>
2023-10-29 03:34:26 +0100

    Adding explosion to grenade.

--- Assets/Scripts/Weapon/StuffedRabbit/StuffedRabbit.cs
+++ Assets/Scripts/Weapon/StuffedRabbit/StuffedRabbit.cs
@@ -1,3 +1,4 @@
+using RayFire;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,35 +8,46 @@ public class StuffedRabbit : MonoBehaviour
     [Header("References")]
     [SerializeField] private GunData grenadeData;
     [SerializeField] private GameObject explosionParticles;
+    private RayfireBomb rayfireBomb;
 
     private float countDown;
+    private float explosionCountDown = 0.025f;
     private bool hasExploded;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         countDown = grenadeData.timeBeforeExplosion;
         hasExploded = false;
+        rayfireBomb = GetComponent<RayfireBomb>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-
+    {   
         countDown -= Time.deltaTime;
 
-        if (countDown < 0 && !hasExploded)
+        if (countDown < 0)
         {
-            Explode();
-            hasExploded = true;
-        }
+            if (!hasExploded)
+            {
+                Explode();
+                hasExploded = true;
+            } else
+            {
+                explosionCountDown -= Time.deltaTime;
 
+                if (explosionCountDown < 0)
+                    Destroy(gameObject);
+                else
+                    rayfireBomb.Explode(0);
+            }
+        }
     }
 
     private void Explode()
     {
         Instantiate(explosionParticles, transform.position, Quaternion.identity);
-
         Collider[] colliders = Physics.OverlapSphere(transform.position, grenadeData.range);
 
         foreach(Collider nearbyObject in colliders)
@@ -47,8 +59,6 @@ public class StuffedRabbit : MonoBehaviour
             }
 
         }
-
-        Destroy(gameObject);
     }
 
     private void OnDrawGizmos()

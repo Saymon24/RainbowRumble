b3ff931a783a7fcd31cd1332b9dbeaff447d18ef
Saymon24 <72013481+Saymon24@users.noreply.github.com>
2023-10-31 10:32:55 +0900

    [+] Add file for save (WiP)

---
+++ Assets/Scripts/Save/SaveData.cs
@@ -0,0 +1,10 @@
+
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SaveData
+{
+    public List<ShopData> shop;
+    /*public List<MapData> maps;*/
+    /*public profileData profile;*/
+}
---
+++ Assets/Scripts/Shop/ShopWeapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class ShopWeapon : ScriptableObject
+{
+
+    public string weaponName;
+    public List<int> upgradesLevel;
+    public List<string> skins;
+
+}

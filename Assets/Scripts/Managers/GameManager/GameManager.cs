c3d872935ac76ec0aef6f2b7a6126cab77ac859d
Alexis Devloo <72014883+AlexisDevloo@users.noreply.github.com>
2023-11-02 12:58:14 +0100

    Adding Play cycle in game and fix some bugs like candy power up for plane

--- Assets/Scripts/Managers/GameManager/GameManager.cs
+++ Assets/Scripts/Managers/GameManager/GameManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject player;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!player.GetComponent<PlayerManager>().isAlive)
+        {
+            LoadingManager.instance.changeNextSceneToLoad("MainMenu");
+            SceneManager.LoadScene("LoadingScene");
+        }
     }
 }
--- Assets/Scripts/Menus/MainMenu/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void GoToSelectionMenu()
     {
         LoadingManager.instance.changeNextSceneToLoad("EnemiesDebug");

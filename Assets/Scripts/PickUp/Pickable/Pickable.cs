186f4381b29ee4a142d5319e4e4f6be2016c1426
Alexis Devloo <72014883+AlexisDevloo@users.noreply.github.com>
2023-11-02 08:06:46 +0100

    Destroy power up

--- Assets/Scripts/PickUp/Pickable/Pickable.cs
+++ Assets/Scripts/PickUp/Pickable/Pickable.cs
@@ -6,9 +6,24 @@ using UnityEngine.Events;
 
 public class Pickable : MonoBehaviour
 {
+    [SerializeField] float despawnTime = 60.0f;
+    private float startTime = 0;
     private bool isPickUp { set; get; } = false;
 
     [SerializeField] private UnityEvent pickupCallback;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float elapsedTime = Time.time - startTime;
+
+        if (elapsedTime >= despawnTime)
+            Destroy(gameObject);
+    }
 
     private void OnTriggerEnter(Collider other)
     {

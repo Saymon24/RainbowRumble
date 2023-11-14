ab24335e7c3cbb4a73a041682f45245898907e20
Saymon24 <72013481+Saymon24@users.noreply.github.com>
2023-11-14 11:34:49 +0900

    [+] Death Menu

--- Assets/Scripts/Menus/DeathMenu/DeathMenu.cs
+++ Assets/Scripts/Menus/DeathMenu/DeathMenu.cs
@@ -1,18 +1,79 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Texts")]
+    [SerializeField] private TMP_Text ScoreTxt;
+    [SerializeField] private TMP_Text TCTxt;
+    [SerializeField] private TMP_Text RCTxt;
+
+    private ScoreManager Score;
+    private int TC;
+    private int RC;
+    private List<Transform> children;
+
+    private void Start()
     {
-        
+        Score = GameObject.Find("GameManager").GetComponent<ScoreManager>();
+
+        children = new List<Transform>();
+
+        foreach (Transform t in transform)
+        {
+            children.Add(t);
+            t.gameObject.SetActive(false); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeathScreen()
     {
-        
+        Time.timeScale = 0.0f;
+
+        foreach (Transform t in children)
+        {
+            t.gameObject.SetActive(true);
+        }
+
+        ScoreTxt.text = "Score: " + Score.score;
+        CalculateCoins(Score.score);
+
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    private void CalculateCoins(int score)
+    {
+        TC = score / 10_000;
+        TCTxt.text = "TC: " + TC.ToString();
+
+        if (score >= 1_000_000)
+        {
+            if (Random.Range(0, 100) == 0)
+            {
+                RC = Random.Range(1, 5);
+            } else
+            {
+                RC = 0;
+            }
+        }
+        RCTxt.text = "RC: " + RC.ToString();
+    }
+
+    public void GoBackToMainMenu()
+    {
+        LoadingManager.instance.changeNextSceneToLoad("MainMenu");
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    public void TryAgain()
+    {
+        LoadingManager.instance.changeNextSceneToLoad("Terrain");
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("LoadingScene");
     }
 }
--- Assets/Scripts/Menus/MainMenu/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {

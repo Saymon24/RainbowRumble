f329bb4855d7cc2e246f61cfc3003886e3db67e1
Alexis Devloo <72014883+AlexisDevloo@users.noreply.github.com>
2023-11-14 08:04:57 +0100

    Merge some gameplay elements in alphaLevel

--- Assets/Scripts/Menus/MainMenu/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public void GoToSelectionMenu()
     {
-        LoadingManager.instance.changeNextSceneToLoad("EnemiesDebug");
+        LoadingManager.instance.changeNextSceneToLoad("AlphaLevel");
         SceneManager.LoadScene("LoadingScene");
     }
 

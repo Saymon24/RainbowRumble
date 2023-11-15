825f9fe56839795e9adff8b927eab6b24b05d216
Eliott Avetand <eliott.avetand@epitech.eu>
2023-11-15 15:05:12 +0900

    [+] Added the changed of weapon script

--- Assets/Scripts/Interface/Weapons.cs
+++ Assets/Scripts/Interface/Weapons.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class Weapons : MonoBehaviour
 {
-    private GunData _weaponData;
-    private string _oldWeaponName = "";
+    private List<GunData> _weaponsData;
+    private string _oldWeaponMainName = "";
+    private string _oldWeaponSubName = "";
 
     [SerializeField] private Image _mainWeapon;
     [SerializeField] private Image _subWeapon;
@@ -18,17 +19,36 @@ public class Weapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _weaponData = GameObject.Find("Player").GetComponentInChildren<WeaponHolder>().GetWeaponData();
+        _weaponsData = GameObject.Find("Player").GetComponentInChildren<WeaponHolder>().GetWeaponsData();
 
-        if (_weaponData)
+        if (HasSubChanged())
         {
-            if (_weaponData.name != _oldWeaponName && _oldWeaponName != "")
+            if (_oldWeaponSubName == "")
             {
-                Sprite tmp = _mainWeapon.sprite;
-                _mainWeapon.sprite = _subWeapon.sprite;
-                _subWeapon.sprite = tmp;
+                _subWeapon.sprite = Resources.Load<Sprite>(_weaponsData[1].name);
+                _oldWeaponSubName = _weaponsData[1].name;
+            } else
+            {
+                if (_oldWeaponSubName == _oldWeaponMainName)
+                {
+                    Sprite tmp = _mainWeapon.sprite;
+                    _mainWeapon.sprite = _subWeapon.sprite;
+                    _subWeapon.sprite = tmp;
+                } else
+                {
+                    _mainWeapon.sprite = _subWeapon.sprite;
+                    _subWeapon.sprite = Resources.Load<Sprite>(_weaponsData[1].name);
+                    _oldWeaponSubName = _weaponsData[1].name;
+                }
             }
-            _oldWeaponName = _weaponData.name;
         }
+        _oldWeaponMainName = _weaponsData[0].name;
+    }
+
+    private bool HasSubChanged()
+    {
+        if (_weaponsData.Count > 1)
+            return _weaponsData[1].name != _oldWeaponSubName;
+        else return false;
     }
 }

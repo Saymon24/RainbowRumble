c1e790e5595e0d471f5e1c2e05205b46cc24dc89
Alexis Devloo <72014883+AlexisDevloo@users.noreply.github.com>
2023-12-10 12:55:57 +0100

    Fix some bugs

--- Assets/Scripts/Menus/MainMenu/MenuButtonHover.cs
+++ Assets/Scripts/Menus/MainMenu/MenuButtonHover.cs
@@ -6,18 +6,20 @@ using UnityEngine.InputSystem;
 
 public class MenuButtonHover : MonoBehaviour
 {
+    public bool isOnRight = false;
+
     private void Update()
     {
     }
     public void OnPointerEnter( )
     {
         Debug.Log("enter");
-        transform.position = new Vector3(transform.position.x + 10, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x + (!isOnRight ? 10 : -10), transform.position.y, transform.position.z);
     }
 
     public void OnPointerExit( )
     {
         Debug.Log("exit");
-        transform.position = new Vector3(transform.position.x - 10, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x - (!isOnRight ? 10 : -10), transform.position.y, transform.position.z);
     }
 }
